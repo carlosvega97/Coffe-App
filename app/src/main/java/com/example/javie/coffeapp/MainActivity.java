3d757b8a3ada235b489b719201640ef5494cb11e
Javier <javieerperez@hotmail.com>
2018-01-23 19:42:27 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/javie/coffeapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.javie.coffeapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+}
