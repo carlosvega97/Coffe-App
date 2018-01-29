36f6b53cbdbf1ae9e28aaee90b1fa77057b1dec2
Javier <javieerperez@hotmail.com>
2018-01-29 18:51:12 +0100

    Register Implementation and color palette edition

--- app/src/main/java/com/example/javie/coffeapp/SignUpActivity.java
+++ app/src/main/java/com/example/javie/coffeapp/SignUpActivity.java
@@ -8,15 +8,12 @@ package com.example.javie.coffeapp;
         import android.view.View;
         import android.widget.Button;
         import android.widget.EditText;
-        import android.widget.TextView;
         import android.widget.Toast;
 
         import com.google.android.gms.tasks.OnCompleteListener;
         import com.google.android.gms.tasks.Task;
         import com.google.firebase.auth.AuthResult;
         import com.google.firebase.auth.FirebaseAuth;
-
-        import java.net.PasswordAuthentication;
 
 public class SignUpActivity extends AppCompatActivity {
 
