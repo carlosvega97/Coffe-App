5c2e00e04709efb3c85ed31546a203110c413ad5
Javier <javieerperez@hotmail.com>
2018-02-01 15:52:03 +0100

    update commit =)

--- app/src/main/java/com/example/javie/coffeapp/LoginActivity.java
+++ app/src/main/java/com/example/javie/coffeapp/LoginActivity.java
@@ -1,24 +1,43 @@
 package com.example.javie.coffeapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     protected TextView mSignUpTextView;
     protected Button mLoginButtom;
+
+    protected EditText editTextEmail;
+    protected EditText editTextPass;
+
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth= FirebaseAuth.getInstance();
+
         mSignUpTextView = (TextView)findViewById(R.id.signUpButton);
         mLoginButtom = (Button) findViewById(R.id.loginButton);
-
+        editTextEmail = findViewById(R.id.etEmail1);
+        editTextPass = findViewById(R.id.etPass1);
 
         getSupportActionBar().hide();
 
@@ -32,8 +51,29 @@ public class LoginActivity extends AppCompatActivity {
         mLoginButtom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                startActivity(intent);
+               LoguearUser();
+            }
+        });
+    }
+
+    private void LoguearUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String pwd = editTextPass.getText().toString().trim();
+        mAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.v("Login: ", "signInWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(intent);
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.v("Login", "signInWithEmail:failure", task.getException());
+                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
--- app/src/main/java/com/example/javie/coffeapp/SignUpActivity.java
+++ app/src/main/java/com/example/javie/coffeapp/SignUpActivity.java
@@ -1,25 +1,29 @@
 package com.example.javie.coffeapp;
 
-        import android.app.ProgressDialog;
-        import android.support.annotation.NonNull;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.text.TextUtils;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.Toast;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-        import com.google.android.gms.tasks.OnCompleteListener;
-        import com.google.android.gms.tasks.Task;
-        import com.google.firebase.auth.AuthResult;
-        import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
 
     EditText mNameField;
     EditText mEmailField;
     EditText mPasswordField;
+
+    TextView miReturnLogin;
 
     Button mRegisterButton;
 
@@ -36,6 +40,7 @@ public class SignUpActivity extends AppCompatActivity {
         mEmailField = findViewById(R.id.et_email);
         mPasswordField = findViewById(R.id.et_pass);
         mRegisterButton = findViewById(R.id.btn_register);
+        miReturnLogin = findViewById(R.id.txtvwLog);
 
         mProgess = new ProgressDialog(this);
 
@@ -43,6 +48,13 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startRegister();
+            }
+        });
+        miReturnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
             }
         });
 
