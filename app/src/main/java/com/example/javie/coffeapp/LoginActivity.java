6a1ece693dbc8d9a6f6d7ebdf3bd897543157210
Javier <javieerperez@hotmail.com>
2018-02-22 16:28:36 +0100

    commit with photo picker

--- app/src/main/java/com/example/javie/coffeapp/LoginActivity.java
+++ app/src/main/java/com/example/javie/coffeapp/LoginActivity.java
@@ -9,6 +9,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,9 +31,11 @@ public class LoginActivity extends AppCompatActivity {
     protected TextView mSignUpTextView;
     protected  TextView mRmPass;
     protected Button mLoginButtom;
+    protected Button mCancelButtom;
 
     protected EditText editTextEmail;
     protected EditText editTextPass;
+
 
     ProgressDialog mProgess;
 
@@ -48,6 +51,7 @@ public class LoginActivity extends AppCompatActivity {
 
         mSignUpTextView = (TextView)findViewById(R.id.signUpButton);
         mLoginButtom = (Button) findViewById(R.id.loginButton);
+        //mCancelButtom = findViewById(R.id.btn_cancel);
         editTextEmail = findViewById(R.id.etEmail1);
         editTextPass = findViewById(R.id.etPass1);
         mRmPass = findViewById(R.id.txtRmPass);
@@ -73,37 +77,42 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 /*Intent intent = new Intent(LoginActivity.this, ResetPassActivity.class);
                 startActivity(intent);*/
-                createMessageDialogo();
+                AbrirDialogPassword();
             }
         });
     }
 
     private void LoguearUser() {
         final String email = editTextEmail.getText().toString().trim();
-        String pwd = editTextPass.getText().toString().trim();
-        mAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.v("Login: ", "signInWithEmail:success");
-                    FirebaseUser user = mAuth.getCurrentUser();
-                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                    startActivity(intent);
-                    Snackbar.make(relativeLayout, "Welcome" + email, Snackbar.LENGTH_SHORT).show();
+        final String pwd = editTextPass.getText().toString().trim();
 
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.v("Login", "signInWithEmail:failure", task.getException());
-                    Toast.makeText(LoginActivity.this, "Authentication failed.",
-                            Toast.LENGTH_SHORT).show();
-                    Snackbar.make(relativeLayout, "Welcome" + email, Snackbar.LENGTH_SHORT).show();
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(pwd)) {
+            mAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.v("Login: ", "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                        Snackbar.make(relativeLayout, "Welcome" + email, Snackbar.LENGTH_SHORT).show();
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.v("Login", "signInWithEmail:failure", task.getException());
+                        Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                        Snackbar.make(relativeLayout, "Welcome" + email, Snackbar.LENGTH_SHORT).show();
+                    }
                 }
-            }
-        });
+            });
+        } else {
+            Snackbar.make(relativeLayout, "Rellene los campos vacios", Snackbar.LENGTH_SHORT).show();
+        }
     }
 
     @SuppressLint("NewApi")
-    public void createMessageDialogo() {
+    public void AbrirDialogPassword() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         LayoutInflater inflater = this.getLayoutInflater();
--- app/src/main/java/com/example/javie/coffeapp/PerfilActivity.java
+++ app/src/main/java/com/example/javie/coffeapp/PerfilActivity.java
@@ -1,22 +1,67 @@
 package com.example.javie.coffeapp;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+import java.io.File;
+
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class PerfilActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, GotUser {
     TextView tvNavDrawerUser, tvNavDrawerEmail, tvPersonNameProfile, tvEmailProfile, tvPNumberProfile;
     private Database database = new Database();
     private User user;
+
+//***************************************************************************************************************
+private static String APP_DIRECTORY = "MyPictureApp/";
+    private static String MEDIA_DIRECTORY = APP_DIRECTORY + "PictureApp";
+
+    private final int MY_PERMISSIONS = 100;
+    private final int PHOTO_CODE = 200;
+    private final int SELECT_PICTURE = 300;
+
+    //private ImageView mSetImage;
+    private Button mOptionButton;
+    private RelativeLayout mRlView;
+    private CircularImageView mSetImage;
+
+    private String mPath;
+
+//***************************************************************************************************************
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +80,46 @@ public class PerfilActivity extends AppCompatActivity
         tvPersonNameProfile = findViewById(R.id.tvPersonNameProfile);
         tvEmailProfile = findViewById(R.id.tvEmailProfile);
         tvPNumberProfile = findViewById(R.id.tvPNumberProfile);
+        mOptionButton = findViewById(R.id.buttonimg);
+        mRlView = findViewById(R.id.rlView);
+        mSetImage = findViewById(R.id.imageprofile);
+
+        if(myRequestStoragePermission()){
+            mOptionButton.setEnabled(true);
+        }else{
+            mOptionButton.setEnabled(false);
+        }
+
+        mOptionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showOptions();
+            }
+        });
+    }
+
+    private boolean myRequestStoragePermission() {
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return true;
+        if ((checkSelfPermission(WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) &&
+                (checkSelfPermission(CAMERA) == PackageManager.PERMISSION_GRANTED))
+            return true;
+
+        if((shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE)) || (shouldShowRequestPermissionRationale(CAMERA))){
+            Snackbar.make(mRlView, "Los permisos son necesarios para poder usar la app",
+                    Snackbar.LENGTH_INDEFINITE).setAction(android.R.string.ok, new View.OnClickListener() {
+                @TargetApi(Build.VERSION_CODES.M)
+                @Override
+                public void onClick(View v) {
+                    requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE, CAMERA}, MY_PERMISSIONS);
+                }
+            }).show();
+        } else {
+            requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE, CAMERA}, MY_PERMISSIONS);
+        }
+
+        return false;
     }
 
     @Override
@@ -46,6 +131,7 @@ public class PerfilActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -107,6 +193,134 @@ public class PerfilActivity extends AppCompatActivity
         tvPersonNameProfile.setText(user.getPersonName());
         tvEmailProfile.setText(user.getEmail());
         tvPNumberProfile.setText(user.getPnumber());
+    }
+
+    @SuppressLint("NewApi")
+    public void showOptions() {
+        final CharSequence[] option = {"Camara", "Galeria", "Cancelar"};
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(PerfilActivity.this);
+        builder.setTitle("Elige una opción");
+        builder.setItems(option, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+              if (option[which]=="Camara") {
+                  openCamera();
+              }else if(option[which]=="Galeria"){
+                    Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(intent.createChooser(intent, "Selecciona la app de imagenes"), SELECT_PICTURE);
+              } else {
+                  dialog.dismiss();
+              }
+            }
+        });
+        builder.show();
+/*
+        LayoutInflater inflater = this.getLayoutInflater();
+        View v = inflater.inflate(R.layout.activity_imagen_profile_dialog, null);
+        builder.setView(v);
+        builder.create().show();
+*/
+    }
+
+    private void openCamera() {
+        File file = new File(Environment.getExternalStorageDirectory(), MEDIA_DIRECTORY);
+        boolean isDirectoryCreated = file.exists();
+
+        if (!isDirectoryCreated){
+            isDirectoryCreated = file.mkdirs();
+        }
+        if (isDirectoryCreated){
+            Long timestamp = System.currentTimeMillis() / 1000;
+            String imageName = timestamp.toString() + ".png";
+
+            mPath = Environment.getExternalStorageState() + File.separator + MEDIA_DIRECTORY
+                    + File.separator + imageName;
+
+            File newFile = new File(mPath);
+
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(newFile));
+            startActivityForResult(intent, PHOTO_CODE);
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("file_path", mPath);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        mPath = savedInstanceState.getString("file_path");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            switch(requestCode){
+                case PHOTO_CODE:
+                    MediaScannerConnection.scanFile(this,
+                            new String[]{mPath}, null,
+                            new MediaScannerConnection.OnScanCompletedListener() {
+                                @Override
+                                public void onScanCompleted(String path, Uri uri) {
+                                    Log.i("ExternalStorage", "Scanned " + path);
+                                    Log.i("ExternalStorage", "-> Uri = " + uri);
+                                }
+                            });
+                    Bitmap bitmap = BitmapFactory.decodeFile(mPath);
+                    mSetImage.setImageBitmap(bitmap);
+                    break;
+                case SELECT_PICTURE:
+                    Uri path = data.getData();
+                    mSetImage.setImageURI(path);
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == MY_PERMISSIONS) {
+            if (grantResults.length == 2 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(PerfilActivity.this, "Permisos aceptados", Toast.LENGTH_SHORT).show();
+                mOptionButton.setEnabled(true);
+            }
+        } else {
+            showExplanation();
+        }
+    }
+         private void showExplanation(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(PerfilActivity.this);
+        builder.setTitle("PERMISOS DENEGADOS");
+        builder.setMessage("Para usar las funciones de la app necesitas aceptar los permisos");
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                intent.setData(uri);
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                finish();
+            }
+        });
+        builder.show();
     }
 }
 
